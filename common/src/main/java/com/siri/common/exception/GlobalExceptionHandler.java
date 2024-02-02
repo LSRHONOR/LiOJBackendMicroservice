4a9b0f81a67ef00983c41d220ca942f5a2fb64a6
LiSiRui <2763590149@qq.com>
2024-02-03 00:32:30 +0800

    Initial commit

---
+++ common/src/main/java/com/siri/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.siri.common.exception;
+
+import com.siri.common.common.BaseResponse;
+import com.siri.common.common.ErrorCode;
+import com.siri.common.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
